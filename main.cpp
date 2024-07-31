7483985e18820de1d0cbb7e49dc5d17c0f6c2ccf
Progress Team <163340619+ProgressTeamRU@users.noreply.github.com>
2024-07-31 12:38:41 +0300

    Instruction and tips

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
+#include <iostream> //iostream это библиотека (ввод/вывод)
 
-using namespace std;
+using namespace std; //пространство имен
 
-void main()
+void main() //точка входа в систему и место где мы пишем программу
 {
-	cout << "Go to my dick"<< endl;
+	cout << "Hello, World!"<< endl; //в " мы пишем текст который хотим вывести. 
 }
+
+// Чтобы написать на русском нужно в верхней строчке перед Hello, World! написать ( нижняя строчка \/) 
+// setlocale(CL_ALL, "Rus");
